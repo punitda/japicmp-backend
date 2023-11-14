7bdfef33410a9630a13480449da7214cb3fa3831
punitda <punitdama@gmail.com>
2023-11-14 10:50:13 +0530

    Configurate app via config file

--- src/main/kotlin/punitd/dev/Application.kt
+++ src/main/kotlin/punitd/dev/Application.kt
@@ -1,13 +1,11 @@
 package punitd.dev
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import punitd.dev.plugins.*
+import punitd.dev.plugins.configureRouting
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    EngineMain.main(args)
 }
 
 fun Application.module() {
