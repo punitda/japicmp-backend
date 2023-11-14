2dfa8ca052ba83ee472f2f8014e9067a2860a5de
punitda <punitdama@gmail.com>
2023-11-14 10:17:41 +0530

    Initial commit :v:

---
+++ src/test/kotlin/punitd/dev/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package punitd.dev
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Test
+import punitd.dev.plugins.configureRouting
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
