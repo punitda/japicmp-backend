652dcc270f42af1883769c98de750028842f5113
punitda <punitdama@gmail.com>
2023-11-18 18:37:51 +0530

    Add generateReportByFile route

--- src/main/kotlin/punitd/dev/routes/ReportRoutes.kt
+++ src/main/kotlin/punitd/dev/routes/ReportRoutes.kt
@@ -1,5 +1,10 @@
 package punitd.dev.routes
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.S3Object
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -11,19 +16,20 @@ import io.ktor.server.request.*
 import io.ktor.server.resources.post
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import japicmp.cmp.JApiCmpArchive
-import japicmp.config.Options
-import japicmp.model.AccessModifier
-import japicmp.util.Optional
 import kotlinx.serialization.json.Json
 import punitd.dev.manager.FileManager
+import punitd.dev.manager.FileManager.aarToClassesJar
 import punitd.dev.manager.ReportGenerator
+import punitd.dev.models.requestbody.GenerateReportByFilesRequestBody
 import punitd.dev.models.requestbody.GenerateReportByPackageNameRequestBody
 import punitd.dev.repository.MavenRepository
 import punitd.dev.util.Constants
+import punitd.dev.util.EnvConfig
 import punitd.dev.util.MissingFieldException
 import punitd.dev.util.isValidPackageName
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 @Resource("/report")
 private class ReportPath {
@@ -38,6 +44,7 @@ private class ReportPath {
 fun Application.reportRoutes() {
     routing {
         createReportMaven()
+        createReportFile()
     }
 }
 
@@ -121,5 +128,68 @@ fun Route.createReportMaven() {
         // Send HTML Report in response
         val file = outputFiles.first()
         return@post call.respondFile(file)
+    }
+}
+
+fun Route.createReportFile() {
+    val credentials = BasicAWSCredentials(EnvConfig.AWS_ACCESS_KEY, EnvConfig.AWS_SECRET_KEY)
+    val s3 = AmazonS3ClientBuilder.standard()
+        .withCredentials(AWSStaticCredentialsProvider(credentials))
+        .withEndpointConfiguration(
+            AwsClientBuilder.EndpointConfiguration(
+                EnvConfig.AWS_SERVICE_ENDPOINT_R2,
+                EnvConfig.AWS_SIGNING_REGION
+            )
+        ).build()
+
+
+    post<ReportPath.File> {
+        val requestBody = runCatching { call.receiveNullable<GenerateReportByFilesRequestBody>() }.getOrNull()
+            ?: throw MissingFieldException("Missing fields in request body")
+
+        val (oldFileKeyName, oldVersion, newFileKeyName, newVersion) = requestBody
+
+        runCatching {
+            val oldFilePath = "build/${oldFileKeyName}"
+            val newFilePath = "build/${newFileKeyName}"
+            val olds3Object = s3.getObject(EnvConfig.BUCKET_NAME, oldFileKeyName)
+            convertS3ObjectToFile(olds3Object, oldFilePath)
+            val news3Object = s3.getObject(EnvConfig.BUCKET_NAME, newFileKeyName)
+            convertS3ObjectToFile(news3Object, newFilePath)
+
+            val isAar = oldFileKeyName.contains("aar") && newFileKeyName.contains("aar")
+
+            val outputFiles = ReportGenerator.generateReport(
+                oldArtifactFile = if (isAar) aarToClassesJar(File(oldFilePath))!! else File(oldFilePath),
+                oldVersion = oldVersion,
+                newArtifactFile = if (isAar) aarToClassesJar(File(newFilePath))!! else File(newFilePath),
+                newVersion = newVersion
+            )
+
+            // Send HTML Report in response
+            val file = outputFiles.first()
+            return@post call.respondFile(file)
+        }.getOrElse {
+            return@post call.respondText(
+                text = "Unable to generate report",
+                status = HttpStatusCode.InternalServerError
+            )
+        }
+    }
+}
+
+private fun convertS3ObjectToFile(s3Object: S3Object, localFilePath: String) {
+    try {
+        s3Object.objectContent.use { inputStream ->
+            FileOutputStream(localFilePath).use { fileOutputStream ->
+                val buffer = ByteArray(4096)
+                var bytesRead: Int
+                while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead)
+                }
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
 }
