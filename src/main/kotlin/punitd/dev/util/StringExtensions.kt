35ae9faf3083e729b902c0631435fc88ec3865d8
punitda <punitdama@gmail.com>
2023-11-18 18:03:29 +0530

    Add slugify extension function

--- src/main/kotlin/punitd/dev/util/StringExtensions.kt
+++ src/main/kotlin/punitd/dev/util/StringExtensions.kt
@@ -4,3 +4,11 @@ fun String.isValidPackageName(): Boolean {
     val packageRegex = """^([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]+):([\d.]+)$""".toRegex()
     return this.matches(packageRegex)
 }
+
+fun String.slugify(): String {
+    // Replace non-alphanumeric characters with dashes
+    val slug = this.replace(Regex("[^a-zA-Z0-9]"), "-")
+
+    // Remove leading and trailing dashes
+    return slug.trim('-').toLowerCase()
+}
