0b6721437b3a298f169c72288c0f096bf2c4452c
punitda <punitdama@gmail.com>
2023-11-18 10:40:52 +0530

    Fix regex to allow numbers in groupId and artifactId

--- src/main/kotlin/punitd/dev/util/StringExtensions.kt
+++ src/main/kotlin/punitd/dev/util/StringExtensions.kt
@@ -1,6 +1,6 @@
 package punitd.dev.util
 
 fun String.isValidPackageName(): Boolean {
-    val packageRegex = """^([a-zA-Z._-]+):([a-zA-Z._-]+):([\d.]+)$""".toRegex()
+    val packageRegex = """^([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]+):([\d.]+)$""".toRegex()
     return this.matches(packageRegex)
 }
