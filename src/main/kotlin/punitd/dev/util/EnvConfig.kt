c0d97d7d016ae32e14242702b23d57400fd922c3
punitda <punitdama@gmail.com>
2023-11-19 12:03:04 +0530

    Update EnvConfig

--- src/main/kotlin/punitd/dev/util/EnvConfig.kt
+++ src/main/kotlin/punitd/dev/util/EnvConfig.kt
@@ -1,9 +1,9 @@
 package punitd.dev.util
 
-object EnvConfig {
-    val BUCKET_NAME: String = System.getenv("BUCKET_NAME")
-    val AWS_ACCESS_KEY: String = System.getenv("AWS_ACCESS_KEY")
-    val AWS_SECRET_KEY: String = System.getenv("AWS_SECRET_KEY")
-    val AWS_SERVICE_ENDPOINT_R2: String = System.getenv("AWS_SERVICE_ENDPOINT_R2")
-    val AWS_SIGNING_REGION: String = System.getenv("AWS_SIGNING_REGION")
-}
+data class EnvConfig(
+    val AWS_ACCESS_KEY: String,
+    val AWS_SECRET_KEY: String,
+    val AWS_SERVICE_ENDPOINT_R2: String,
+    val AWS_SIGNING_REGION: String,
+    val BUCKET_NAME: String,
+)
