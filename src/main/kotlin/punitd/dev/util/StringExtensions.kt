11cc493865e59dd081aebe113b544de352d7f253
PunitD <punitdama@gmail.com>
2023-12-30 18:01:01 +0530

    Update package name regex (#10)
    To allow alphabets in version because sometimes version could include values like "2.0.0-beta", "2.0.0-rc01", etc.

--- src/main/kotlin/punitd/dev/util/StringExtensions.kt
+++ src/main/kotlin/punitd/dev/util/StringExtensions.kt
@@ -1,7 +1,7 @@
 package punitd.dev.util
 
 fun String.isValidPackageName(): Boolean {
-    val packageRegex = """^([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]+):([\d.]+)$""".toRegex()
+    val packageRegex = """^([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]+)$""".toRegex()
     return this.matches(packageRegex)
 }
 
